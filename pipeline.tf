fbc281e14c5b9afaeb2226380e19aad8d47f1c39
aldrin027 <aldrin.gauran@gmail.com>
2021-07-04 20:46:13 +0800

    change image of terraform from dockerhub

--- pipeline.tf
+++ pipeline.tf
@@ -9,7 +9,7 @@ resource "aws_codebuild_project" "tf-plan" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "hashicorp/terraform:1.1.0-alpha20210630"
+    image                       = "hashicorp/terraform:latest"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
     registry_credential {
@@ -35,7 +35,7 @@ resource "aws_codebuild_project" "tf-apply" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "hashicorp/terraform:1.1.0-alpha20210630"
+    image                       = "hashicorp/terraform:latest"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
     registry_credential {
